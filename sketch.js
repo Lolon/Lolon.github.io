f081e0ba5d02b28e90a59c53427fca3c6d76944a
Lolon <leonphilpot@live.co.uk>
2017-12-06 19:33:36 +0000

    added base framework

---
+++ sketch.js
@@ -0,0 +1,21 @@
+//https://p5js.org
+
+function setup() {
+  createCanvas(594, 841);
+
+}
+
+function draw() {
+  background(160);
+  //BgDio
+  //FgDio
+}
+
+class Diorama {
+  constructor(Name){
+
+  }
+  displayDio(){
+
+  }
+}
