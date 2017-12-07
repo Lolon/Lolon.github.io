dfffda5a3779f11eb10dd8eceed52c18ebf1ebd7
(s) Leon Philpot <lphilpot@pm7-0274.uopnet.plymouth.ac.uk>
2017-12-07 12:30:24 +0000

    added image

--- sketch.js
+++ sketch.js
@@ -1,21 +1,39 @@
 //https://p5js.org
-
+let BeginDiorama;
+let dagger;
 function setup() {
   createCanvas(594, 841);
-
+  BeginDiorama = new Diorama(0);
+  BeachDiorama = new Diorama(1);
 }
 
 function draw() {
-  background(160);
+  background(255);
+  stroke(0,0,0,0);
+    BeginDiorama.displayDio();
   //BgDio
+  BeachDiorama.displayDio();
   //FgDio
+
 }
 
 class Diorama {
-  constructor(Name){
-
+  constructor(DioUnit){
+    this.DioUnit = DioUnit;
   }
-  displayDio(){
-
+   displayDio(){
+    if (this.DioUnit == 0){
+      fill (51);
+      rect (0,0,594,841);
+      dagger = loadImage("images/dagger.png")
+      image(dagger,0,0);
+    }
+    else if (this.DioUnit == 1) {
+      fill (0,255,0);
+      rect (20,20,20,20);
+    }
+    else if(this.DioUnit == undefined){
+      console.log("dioUnit is not defined");
+    }
   }
 }
