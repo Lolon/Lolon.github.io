552e9ff617d5f80a12fa5eda5404b5af17946e48
(s) Leon Philpot <lphilpot@pm7-0274.uopnet.plymouth.ac.uk>
2017-12-07 14:21:55 +0000

    Setting up framework

--- sketch.js
+++ sketch.js
@@ -1,39 +1,71 @@
 //https://p5js.org
-let BeginDiorama;
-let dagger;
+//var startClick;
+function preload(){
+  dagger = loadImage("https://lolon.github.io/images/dagger.png")
+
+}
 function setup() {
   createCanvas(594, 841);
   BeginDiorama = new Diorama(0);
+    BeginDiorama.isTicking = true;
+    BeginDiorama.isDrawing = true;
   BeachDiorama = new Diorama(1);
-}
 
-function draw() {
+
+  //startClick.mouseClicked(Start);
+  }
+
+function draw() {             //every tick
   background(255);
   stroke(0,0,0,0);
-    BeginDiorama.displayDio();
-  //BgDio
-  BeachDiorama.displayDio();
-  //FgDio
+  if (BeginDiorama.isTicking == true){
+    BeginDiorama.DioTick();
+    }
+}
 
+function mouseClicked(){
+  //logic for randomising events
+  console.log("click");
+  BeachDiorama.displayDio();
+  BeginDiorama.BHide = true;
 }
 
 class Diorama {
   constructor(DioUnit){
     this.DioUnit = DioUnit;
+    var BHide = false;
+    let isTicking = true;
+    var isDrawing = true;
   }
-   displayDio(){
+
+  DioTick (){
+    console.log("tick");
+    if (this.isDrawing){
+      this.displayDio();
+    }
+
+  }
+  displayDio(){
+    this.isTicking = true;
     if (this.DioUnit == 0){
       fill (51);
-      rect (0,0,594,841);
-      dagger = loadImage("images/dagger.png")
-      image(dagger,0,0);
+      let beBGX = 594;
+      rect (0,0,beBGX,841);
+      image(dagger,150,75);
+      fill (102,153,255);
+      textSize(100);
+      text("BEGIN",110,675);
     }
     else if (this.DioUnit == 1) {
-      fill (0,255,0);
-      rect (20,20,20,20);
     }
     else if(this.DioUnit == undefined){
       console.log("dioUnit is not defined");
+    }
+  }
+  hideDio(){
+    if (this.DioUnit == 0){
+      this.beBGX =- 2
+      console.log("hiding");
     }
   }
 }
