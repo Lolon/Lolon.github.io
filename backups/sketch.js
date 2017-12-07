4130013484fec6617f3ff778423fcbbe8cf762f1
(s) Leon Philpot <lphilpot@pm7-0274.uopnet.plymouth.ac.uk>
2017-12-07 15:36:41 +0000

    rejigged the code to use mveteanu's scenemanager

---
+++ backups/sketch.js
@@ -0,0 +1,90 @@
+//https://p5js.org
+//var startClick;
+function preload(){
+  dagger = loadImage("https://lolon.github.io/images/dagger.png")
+
+}
+function setup() {
+  createCanvas(594, 841);
+  BeginDiorama = new Diorama(0);
+    BeginDiorama.isTicking = true;
+    BeginDiorama.isDrawing = true;
+    BeginDiorama.beBGX = 594;
+    BeginDiorama.DagX = 150;
+    BeginDiorama.TextX = 110;
+  BeachDiorama = new Diorama(1);
+    BeachDiorama.isTicking = false;
+    BeachDiorama.isDrawing = false;
+  }
+
+function draw() {             //every tick
+  background(255);
+  stroke(0,0,0,0);
+  if (BeginDiorama.isTicking == true){
+    BeginDiorama.DioTick();
+    }
+  if (BeachDiorama.isTicking == true){
+      BeachDiorama.DioTick();
+      }
+
+}
+
+function mouseClicked(){
+  BeachDiorama.isTicking = true;                          //logic for randomising events
+  console.log("click");
+  BeginDiorama.OutAnim = true;
+}
+
+class Diorama {
+  constructor(DioUnit){
+    this.DioUnit = DioUnit;
+    let isTicking = true;
+    var isDrawing = true;
+    let OutAnim = false;
+    let beBGX = 594;
+    let DagX = 150;
+    let TextX = 110;
+  }
+
+  DioTick (){
+    //console.log("tick");
+    if (this.isDrawing){
+      this.displayDio();
+      }
+    if (this.OutAnim){
+      this.hideDio();
+    }
+  }
+
+  displayDio(){
+    this.isTicking = true;
+    if (this.DioUnit == 0){
+      push();
+      fill (51);
+      rect (0,0,this.beBGX,841);
+      image(dagger,this.DagX,75);
+      fill (102,153,255);
+      textSize(100);
+      text("BEGIN",this.TextX,675);
+      pop();
+    }
+    else if (this.DioUnit == 1) {
+      fill(0,255,0)
+      rect (50,50,50,50);
+    }
+    else if(this.DioUnit == undefined){
+      console.log("dioUnit is not defined");
+    }
+  }
+  hideDio(){
+    if (this.DioUnit == 0){
+      if (this.beBGX <-100){
+        this.isTicking = false;
+      }
+      this.beBGX =this.beBGX-10;
+      this.DagX = this.DagX - 5;
+      this.TextX = this.TextX - 12;
+      console.log(this.beBGX);
+    }
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,84 +1,45 @@
-//https://p5js.org
-//var startClick;
+var mgr;
+
 function preload(){
   dagger = loadImage("https://lolon.github.io/images/dagger.png")
 
 }
-function setup() {
-  createCanvas(594, 841);
-  BeginDiorama = new Diorama(0);
-    BeginDiorama.isTicking = true;
-    BeginDiorama.isDrawing = true;
-    BeginDiorama.beBGX = 594;
-    BeginDiorama.DagX = 150;
-    BeginDiorama.TextX = 110;
-  BeachDiorama = new Diorama(1);
-
-
+  function setup()
+  {
+    console.log("setup");
+    createCanvas(594, 841);
+    mgr = new SceneManager();
+    mgr.addScene (Intro);
+    mgr.showScene(Intro); //this will show the scene
+  }
+  function draw(){
+    mgr.draw();
   }
 
-function draw() {             //every tick
-  background(255);
-  stroke(0,0,0,0);
-  if (BeginDiorama.isTicking == true){
-    BeginDiorama.DioTick();
+function Intro()
+{
+    this.setup = function() {
+
     }
-}
 
-function mouseClicked(){
-  //logic for randomising events
-  console.log("click");
-  BeginDiorama.OutAnim = true;
-}
-
-class Diorama {
-  constructor(DioUnit){
-    this.DioUnit = DioUnit;
-    let isTicking = true;
-    var isDrawing = true;
-    let OutAnim = false;
-    let beBGX = 594;
-    let DagX = 150;
-    let TextX = 110;
-  }
-
-  DioTick (){
-    //console.log("tick");
-    if (this.isDrawing){
-      this.displayDio();
-      }
-    if (this.OutAnim){
-      this.hideDio();
-    }
-  }
-
-  displayDio(){
-    this.isTicking = true;
-    if (this.DioUnit == 0){
-      push();
+    this.draw = function() {
       fill (51);
-      rect (0,0,this.beBGX,841);
-      image(dagger,this.DagX,75);
+      rect (0,0,594,841);
+      image(dagger,150,75);
       fill (102,153,255);
       textSize(100);
-      text("BEGIN",this.TextX,675);
-      pop();
+      text("BEGIN",110,675);
     }
-    else if (this.DioUnit == 1) {
+
+
+}
+
+// Main games scene constructor function
+function Game()
+{
+    this.setup = function() {
     }
-    else if(this.DioUnit == undefined){
-      console.log("dioUnit is not defined");
+
+    this.draw = function() {
     }
-  }
-  hideDio(){
-    if (this.DioUnit == 0){
-      if (this.beBGX <-100){
-        this.isTicking = false;
-      }
-      this.beBGX =this.beBGX-10;
-      this.DagX = this.DagX - 5;
-      this.TextX = this.TextX - 12;
-      console.log(this.beBGX);
-    }
-  }
 }
