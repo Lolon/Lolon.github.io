39d0d52cfdad251d33add738f463a9bea274f443
Lolon <leonphilpot@live.co.uk>
2017-12-09 16:27:01 +0000

    added image for Forest diorama

--- sketch.js
+++ sketch.js
@@ -13,6 +13,8 @@ function preload(){
   BluePerson = loadImage("https://lolon.github.io/images/BluePerson.png")
   FgCrowd = loadImage("https://lolon.github.io/images/FgCrowd.png")
   Beach_umbrella_blanket = loadImage("https://lolon.github.io/images/Beach_umbrella_blanket.png")
+  beach_wave = loadImage("https://lolon.github.io/images/beach_wave.png")
+  forest_blood = loadImage("https://lolon.github.io/images/forest_blood.png")
 
 }
 
@@ -29,7 +31,7 @@ function setup(){
     mgr.addScene (city);
     mgr.addScene (end);
 
-    mgr.showScene(Beach); //this will show the scene
+    mgr.showScene(forest); //this will show the scene
     Slide1 = 1000;
     Slide2 = Slide1 + 10000;
     Slide3 = Slide2 + 10000;
@@ -71,11 +73,33 @@ function Beach(){
 
     this.draw = function() {
       background(104,217,245);
+      //sun
+      fill(255,239,55);
+      ellipse(400,150,200,200);
+
+      //sea
       fill(104,149,245);
       rect(0,300,594,450);
+      //beach
       fill(245,183,104);
       rect(0,450,594,450)
-      image(Beach_umbrella_blanket,500,300);
+      //wave
+      tint(255,127);
+      image(beach_wave, 100,350);
+      noTint();
+      image(Beach_umbrella_blanket,200,325);
+      //police line
+      fill(94,94,94);
+      rect(50,650,25,200);
+      rect(250,650,25,200);
+      rect(450,650,25,200);
+      rect(650,650,25,200);
+      fill(255,239,55);
+      rect(0,675,594,75);
+      fill(0);
+      textSize(40);
+      text("POLICE LINE DO NOT CROSS - POLICE LINE DO NOT CROSS",0,725);
+      //594, 841
     }
 }
 
@@ -118,8 +142,20 @@ function Crowd(){
   }
   this.draw = function(){
     background(99,122,110);
-    image(FgCrowd,130,250,153*0.75,411*0.75);
-    image(PinkPerson,130,250,65*1.5,176*1.5);
+    //far
+    tint(255,50);
+    image(FgCrowd,130,100,153*0.4,411*0.4);
+    noTint();
+    //closer
+    tint(255,150);
+    image(FgCrowd,130,200,153*0.5,411*0.5);
+    noTint();
+    //close
+    image(FgCrowd,130,300,153*0.75,411*0.75);
+    image(PinkPerson,130,350,65*1.5,176*1.5);
+    //fg crowd
+    tint(200);
+    image(FgCrowd,130,450,153*0.9,411*0.9);
   }
 }
 
@@ -130,24 +166,42 @@ function forest(){
   this.draw = function(){
     background(72,238,235);
     fill(35,49,29);
-    rect(0,400,594,500);
+    rect(0,500,594,500);
     fill(51,38,35);
     quad(150,841,100,841,300,615,350,615);
     quad(250,841,200,841,400,615,450,615);
-    //tree
-    fill(62,53,44);
-    rect(73,250,50,75);
-    fill(49,146,40);
-    triangle(100,0+120,0,150+120,200,150+120);
-    fill(55,164,45);
-    triangle(100,0+80,0,150+80,200,150+80);
-    fill(63,179,52);
-    triangle(100,0+40,0,150+40,200,150+40);
-    fill(73,198,61);
-    triangle(100,0,0,150,200,150);
+    image(forest_blood,0,0);
+    showTree(0,300);
+    showTree(225,300);
+    showTree(525,300);
+    background('rgba(13,51,10,0.1)');
+    showTree(-50,350);
+    showTree(125,350);
+    showTree(425,350);
+    background('rgba(13,51,10,0.1)');
+    //fg
+    showTree(-150,400);
+    showTree(50,400);
+    showTree(325,400);
+    showTree(525,400);
 
 
-  }
+
+}
+
+function showTree(locX,locY,){
+//tree
+fill(62,53,44);
+rect(locX+73,locY+250,50,75);
+fill(49,146,40);
+triangle(locX+100,locY+120,locX+0,locY+150+120,locX+200,locY+150+120);
+fill(55,164,45);
+triangle(locX+100,locY+80,locX+0,locY+150+80,locX+200,locY+150+80);
+fill(63,179,52);
+triangle(locX+100,locY+40,locX+0,locY+150+40,locX+200,locY+150+40);
+fill(73,198,61);
+triangle(locX+100,locY+0,locX+0,locY+150,locX+200,locY+150);
+}
 }
 
 function escape(){
